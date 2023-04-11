4047f5ee4ffa441ac5571eb1e78f3f137eddf15e
Bobby <abuoyn@gmail.com>
2023-04-11 17:37:44 +0800

    update: uni-app

--- src/pages/hello/index.vue
+++ src/pages/hello/index.vue
@@ -1,24 +1,30 @@
 <template>
-    <view class="flex justify-center items-center flex-col">
-    {{title}}
-        <image
-            class="h-200rpx w-200rpx mt-200rpx mb-200rpx mx-auto"
-            src="/static/logo.png"
-        />
-        <view class="text-area">
-            <text class="text-size-36rpx text-gray-400">{{ title }}</text>
-        </view>
+  <view class="flex justify-center items-center flex-col">
+    <image
+      class="h-200rpx w-200rpx mt-200rpx mb-200rpx mx-auto"
+      src="./static/logo.png"
+    />
+    <view class="text-area">
+      <text class="text-size-36rpx text-gray-400">{{ title }}</text>
     </view>
+    <button @tap="toSecond">to second</button>
+  </view>
 </template>
 
 <script setup lang="ts">
-const title = ref('Hello');
+const title = ref("Hello");
 // const title2 = $ref('hello2');
+
+function toSecond() {
+  uni.navigateTo({
+    url: "./index",
+  });
+}
 </script>
 
 <style lang="scss">
 .text-area {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/pages/second/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="flex justify-center items-center flex-col">
+    {{ title }}
+  </view>
+</template>
+
+<script setup lang="ts">
+const title = ref("second");
+// const title2 = $ref('hello2');
+</script>
+
+<style lang="scss">
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+</style>
